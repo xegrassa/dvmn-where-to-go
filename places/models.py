2dd368821fd8a576d30436d93214fa33a6c0909f
Артем Нетрибейчук <artem.netribeychuk@skillbox.ru>
2024-05-29 17:42:00 +0500

    Add Place model

--- places/models.py
+++ places/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class Place(models.Model):
+    title = models.CharField(max_length=100)
+    description_short = models.CharField(max_length=255)
+    description_long = models.TextField()
+    longitude = models.FloatField()
+    latitude = models.FloatField()
