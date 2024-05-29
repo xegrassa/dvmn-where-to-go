708a5254ff82af9bfe36fe3a95abe12627408908
Артем Нетрибейчук <artem.netribeychuk@skillbox.ru>
2024-05-29 22:49:00 +0500

    Add model for images

--- places/admin.py
+++ places/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Place
+from .models import Place, Image
 
 admin.site.register(Place)
+admin.site.register(Image)
--- places/models.py
+++ places/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 
-# Create your models here.
 
 class Place(models.Model):
     title = models.CharField(max_length=100)
@@ -8,3 +7,18 @@ class Place(models.Model):
     description_long = models.TextField()
     longitude = models.FloatField()
     latitude = models.FloatField()
+
+    def __str__(self):
+        return self.title
+
+
+class Image(models.Model):
+    place = models.ForeignKey(Place, null=True, on_delete=models.SET_NULL)
+    image = models.ImageField()
+    _order = models.PositiveSmallIntegerField(verbose_name="order", default=0)
+
+    class Meta(object):
+        ordering = ("place", "_order",)
+
+    def __str__(self):
+        return f"{self._order} {self.place}"
