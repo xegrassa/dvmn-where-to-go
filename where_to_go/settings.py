568a3fdccb6d4595f10a530ceb4a7841686085bd
Артем Нетрибейчук <artem.netribeychuk@skillbox.ru>
2024-05-31 12:11:02 +0500

    Setup media files serving

--- where_to_go/settings.py
+++ where_to_go/settings.py
@@ -114,10 +114,16 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static/'),
+    os.path.join(BASE_DIR, 'static/images'),
+    os.path.join(BASE_DIR, 'static/styles'),
+    os.path.join(BASE_DIR, 'static/scripts'),
+    os.path.join(BASE_DIR, 'static/places'),
 ]
+
+MEDIA_URL = "/media/"
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
