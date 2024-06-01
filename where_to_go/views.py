f28c5f764ba6564439a745bde8255a26da3d7ca1
Артем Нетрибейчук <artem.netribeychuk@skillbox.ru>
2024-06-01 20:14:51 +0500

    Move data from template to view

--- where_to_go/views.py
+++ where_to_go/views.py
@@ -1,5 +1,34 @@
 from django.shortcuts import render
 
-def show_main(request):
-    return render(request, 'index.html')
 
+def show_main(request):
+    points = {
+        "type": "FeatureCollection",
+        "features": [
+            {
+                "type": "Feature",
+                "geometry": {
+                    "type": "Point",
+                    "coordinates": [37.62, 55.793676]
+                },
+                "properties": {
+                    "title": "«Легенды Москвы",
+                    "placeId": "moscow_legends",
+                    "detailsUrl": "./static/moscow_legends.json"
+                }
+            },
+            {
+                "type": "Feature",
+                "geometry": {
+                    "type": "Point",
+                    "coordinates": [37.64, 55.753676]
+                },
+                "properties": {
+                    "title": "Крыши24.рф",
+                    "placeId": "roofs24",
+                    "detailsUrl": "./static/roofs24.json"
+                }
+            }
+        ]
+    }
+    return render(request, 'index.html', context={"points": points})
