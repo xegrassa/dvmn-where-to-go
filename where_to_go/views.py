d3a616f0c28bee9ee6ed427e9adc01856d36a840
Артем Нетрибейчук <artem.netribeychuk@skillbox.ru>
2024-06-10 22:11:28 +0500

    Info about places getting from DB

--- where_to_go/views.py
+++ where_to_go/views.py
@@ -1,34 +1,27 @@
 from django.shortcuts import render
+from places.models import Place
 
 
 def show_main(request):
+
     points = {
         "type": "FeatureCollection",
-        "features": [
-            {
-                "type": "Feature",
-                "geometry": {
-                    "type": "Point",
-                    "coordinates": [37.62, 55.793676]
-                },
-                "properties": {
-                    "title": "«Легенды Москвы",
-                    "placeId": "moscow_legends",
-                    "detailsUrl": "./static/moscow_legends.json"
-                }
-            },
-            {
-                "type": "Feature",
-                "geometry": {
-                    "type": "Point",
-                    "coordinates": [37.64, 55.753676]
-                },
-                "properties": {
-                    "title": "Крыши24.рф",
-                    "placeId": "roofs24",
-                    "detailsUrl": "./static/roofs24.json"
-                }
-            }
-        ]
+        "features": []
     }
+
+    for place in Place.objects.all():
+        point = {
+            "type": "Feature",
+            "geometry": {
+                "type": "Point",
+                "coordinates": [place.longitude, place.latitude]
+            },
+            "properties": {
+                "title": place.title,
+                "placeId": place.id,
+                "detailsUrl": "./static/empty.json"
+            }
+        }
+        points["features"].append(point)
+
     return render(request, 'index.html', context={"points": points})
