7ca2523c81c7b322a65be6528bedb9d797f03431
Артем Нетрибейчук <artem.netribeychuk@skillbox.ru>
2024-06-15 12:10:50 +0500

    View places/<int>/ return full information about Place

--- places/views.py
+++ places/views.py
@@ -1,12 +1,19 @@
 from places.models import Place
-from django.http import HttpResponse
+from django.http import JsonResponse
 from django.shortcuts import get_object_or_404
 
 
-# Create your views here.
-
-
 def place_read(request, place_id: int):
+    place = get_object_or_404(Place, id=place_id)
 
-    obj = get_object_or_404(Place, id=place_id)
-    return HttpResponse(obj.title)
+    payload = {
+        "title": place.title,
+        "imgs": [img.image.url for img in place.image_set.all()],
+        "description_short": place.description_short,
+        "description_long": place.description_long,
+        "coordinates": {
+            "lat": place.latitude,
+            "lng": place.longitude
+        },
+    }
+    return JsonResponse(payload, json_dumps_params={"indent": 4, "ensure_ascii": False}, safe=False)
