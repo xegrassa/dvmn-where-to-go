a8973a2d0082585aa2482fd8e750ae3b4698f867
Артем Нетрибейчук <artem.netribeychuk@skillbox.ru>
2024-06-15 16:17:20 +0500

    Add preview for Image

--- places/admin.py
+++ places/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
+from django.utils.html import format_html
 
 from .models import Place, Image
+
+PREVIEW_IMAGE_HEIGHT = "200"
 
 
 @admin.register(Image)
@@ -12,6 +15,15 @@ class ImageInline(admin.TabularInline):
     model = Image
     verbose_name = "Фотография"
     verbose_name_plural = "Фотографии"
+
+    readonly_fields = ('preview',)
+
+    def preview(self, obj):
+        return format_html(
+            '<img src="{url}" height={height} />',
+            url=obj.image.url,
+            height=PREVIEW_IMAGE_HEIGHT,
+        )
 
 
 @admin.register(Place)
