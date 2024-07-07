0ed6bad24342bffab0edc7f68e32b4148de65adf
Артем Нетрибейчук <artem.netribeychuk@skillbox.ru>
2024-07-07 13:16:43 +0500

    ALLOWED_HOSTS move to env

--- where_to_go/settings.py
+++ where_to_go/settings.py
@@ -10,7 +10,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = env.str('SECRET_KEY')
 DEBUG = env.bool('DEBUG', False)
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = env.list("ALLOWED_HOSTS", default=['127.0.0.1'])
 
 INSTALLED_APPS = [
     "django.contrib.admin",
@@ -20,6 +20,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "places.apps.PlacesConfig",
+
     "adminsortable2",
     "tinymce",
 ]
