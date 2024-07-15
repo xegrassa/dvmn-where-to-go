382d4a9f57688d47867eff48a1be072ddde3125d
Артем Нетрибейчук <artem.netribeychuk@skillbox.ru>
2024-07-15 21:13:51 +0500

    Change static in urls.py

--- where_to_go/urls.py
+++ where_to_go/urls.py
@@ -12,8 +12,7 @@ urlpatterns = [
     path("places/<int:place_id>/", places_view.read_place, name="place_detail"),
     path("", places.views.show_main),
     path("tinymce/", include("tinymce.urls")),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
     urlpatterns += debug_toolbar_urls()
